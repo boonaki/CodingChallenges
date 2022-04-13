d8ca897e155750b56940c132831948da18bdec3e
boonaki <91851563+boonaki@users.noreply.github.com>
2022-04-13 19:09:03 -0400

    Added solution to a coding challenge for 04-13

---
+++ CC2022-04-13/app.js
@@ -0,0 +1,10 @@
+//Write a function that returns a string in which firstname is swapped with last name.
+
+function nameShuffler(str){
+    let arr = str.split(' ')
+    let elem = arr.shift()
+    arr.push(elem)
+    return arr.join(' ')
+}
+
+console.log('john appleseed') //should equal 'appleseed john'
