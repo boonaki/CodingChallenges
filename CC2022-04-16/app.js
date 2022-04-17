885b35f505a7aaf2ae5b95830855f4a4f3e07e51
Joshua Durrett <91851563+boonaki@users.noreply.github.com>
2022-04-16 23:57:28 -0400

    Create app.js

---
+++ CC2022-04-16/app.js
@@ -0,0 +1,8 @@
+//Given a number n, return the number of positive odd numbers below n
+
+function oddCount(n){
+  for(let i = 1;
+}
+
+console.log(oddCount(15)) ///should output
+console.log(oddCount(15023)) ///should output
