8b2fef0cbffbfa6ab4de798f30e61c5f327db4a7
Joshua Durrett <91851563+boonaki@users.noreply.github.com>
2022-04-17 00:00:40 -0400

    Update app.js
    added solution to Coding challenge for 04-06

--- CC2022-04-16/app.js
+++ CC2022-04-16/app.js
@@ -1,8 +1,8 @@
 //Given a number n, return the number of positive odd numbers below n
 
 function oddCount(n){
-  for(let i = 1;
+  return Math.floor(n/2)
 }
 
-console.log(oddCount(15)) ///should output
-console.log(oddCount(15023)) ///should output
+console.log(oddCount(15)) ///should output 7
+console.log(oddCount(15023)) ///should output 7551
