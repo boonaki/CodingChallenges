807e81f78d40a0f6c6a6e7af80535bca5b46853f
boonaki <91851563+boonaki@users.noreply.github.com>
2022-04-24 16:44:51 -0400

    added solution to coding challenge for 04-24

---
+++ CC2022-04-24/app.js
@@ -0,0 +1,17 @@
+//There was a test in your class and you passed it. Congratulations!
+//But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+//You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+//Return True if you're better, else False!
+
+
+function betterThanAverage(classPoints, yourPoints) {
+    return (classPoints.reduce((acc, current) => acc + current, 0) / classPoints.length ) < yourPoints
+}
+
+console.log(betterThanAverage([2, 3], 5)) //should output true
+console.log(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75)) //should output true
+console.log(betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90], 69)) //should output true
+console.log(betterThanAverage([41, 75, 72, 56, 80, 82, 81, 33], 50)) //should output false
+console.log(betterThanAverage([29, 55, 74, 60, 11, 90, 67, 28], 21)) //should output false
