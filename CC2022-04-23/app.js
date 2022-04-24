a50a0a1641e373a31b60a027760c8cf2c8653cfa
boonaki <91851563+boonaki@users.noreply.github.com>
2022-04-23 23:17:34 -0400

    modified app.js

--- CC2022-04-23/app.js
+++ CC2022-04-23/app.js
@@ -4,24 +4,6 @@
 
 //Return their respective ages now as [humanYears,catYears,dogYears]
 
-//NOTES:
-
-    //humanYears >= 1
-    //humanYears are whole numbers only
-
-//Cat Years
-
-    //15 cat years for first year
-    //+9 cat years for second year
-    //+4 cat years for each year after that
-
-//Dog Years
-
-    //15 dog years for first year
-    //+9 dog years for second year
-    //+5 dog years for each year after that
-
-
 
 function humanYearsCatYearsDogYears(humanYears) {
     let catYears = 0
