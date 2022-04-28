d31afd93a3ba65db0dca7362b13b925ce5702cf4
boonaki <91851563+boonaki@users.noreply.github.com>
2022-04-27 22:53:51 -0400

    added solution to third coding challenge for 04-27

--- CC2022-04-27/app02.js
+++ CC2022-04-27/app02.js
@@ -6,3 +6,14 @@
 // Task
 
 // Given the a list of numbers, return a fixed list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included). 
+function pipeFix(numbers){
+    let fixedPipes = []
+    for(let i = numbers[0]; i < numbers[numbers.length-1] + 1; i++){
+      fixedPipes.push(i)
+    }
+    return fixedPipes
+}
+
+console.log(pipeFix([1,2,3,5,6,8,9])) //should output [1,2,3,4,5,6,7,8,9]
+console.log(pipeFix([1,2,3,12])) //should output [1,2,3,4,5,6,7,8,9,10,11,12]
+console.log(pipeFix([6,9])) //should output [6,7,8,9]
