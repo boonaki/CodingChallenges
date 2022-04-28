f79564d5e5af27c8a4673f760e04be2ee9b27449
boonaki <91851563+boonaki@users.noreply.github.com>
2022-04-27 23:21:01 -0400

    added solution to fourth coding challenge for 04-27

---
+++ CC2022-04-27/app03.js
@@ -0,0 +1,26 @@
+//Rock Paper Scissors
+
+//Let's play! You have to return which player won! In case of a draw return Draw!.
+
+function rps(p1, p2){
+    let r = 'r'
+    let p = 'p'
+    let s = 's'
+    p1 = p1.split('')
+    p1 = p1[0]
+    p2 = p2.split('')
+    p2 = p2[0]
+    if(p1 === 'r' && p2 === 's' || p1 === 'p' && p2 === 'r' || p1 === 's' && p2 === 'p'){
+      return 'Player 1 won!'
+    }else if(p2 === 'r' && p1 === 's' || p2 === 'p' && p1 === 'r' || p2 === 's' && p1 === 'p'){
+      return 'Player 2 won!'
+    }else{
+      return 'Draw!'
+    }
+}
+
+console.log(rps('rock', 'scissors')) //should output 'Player 1 won!'
+console.log(rps('scissors', 'paper')) //should output 'Player 1 won!'
+console.log(rps('scissors', 'rock')) //should output 'Player 2 Won!'
+console.log(rps('paper', 'scissors')) //should output 'Player 2 Won!'
+console.log(rps('rock', 'rock')) //should output 'Draw!'
