c7bd878c144e6c026d9e8eb37dbdcc97b1934e54
boonaki <91851563+boonaki@users.noreply.github.com>
2022-04-29 11:44:59 -0400

    added solution to third coding challenge for 04-29

---
+++ CC2022-04-29/app03.js
@@ -0,0 +1,9 @@
+//Given a number n, return the number of positive odd numbers below n
+
+function oddCount(num){
+  return Math.floor(num/2)
+}
+
+console.log(oddCount(12)) //should output 6
+console.log(oddCount(15023)) //should output 7511
+
