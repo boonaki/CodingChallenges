d46f3867f291a61f3b89a2799efc0d86eeea609f
boonaki <91851563+boonaki@users.noreply.github.com>
2022-05-13 15:12:19 -0400

    added solution to coding challenge

---
+++ Cc2022-05-13/booltostr.js
@@ -0,0 +1,10 @@
+//Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+function booleanToString(b){
+    return b ? `true` : `false`
+}
+
+console.log(booleanToString(true)) //should output 'true'
+console.log(booleanToString(false)) //should output 'false'
