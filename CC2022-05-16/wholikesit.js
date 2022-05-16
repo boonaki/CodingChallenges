f53d1d0c81a7634f3233bfc262d7f12d72fb62b1
boonaki <91851563+boonaki@users.noreply.github.com>
2022-05-16 14:57:50 -0400

    added solution to coding challenge

--- CC2022-05-16/wholikesit.js
+++ CC2022-05-16/wholikesit.js
@@ -10,4 +10,33 @@
 
 // Note: For 4 or more names, the number in "and 2 others" simply increases.
 
+//P: an array of strings, always be strings, always be in the same format (capitalization)
+//R: return a new string with the names of the people that like it
+//E:
+
+function likes(name){
+    //names.length === 1 return basic string
+    //names.length === 2 return and string
+    //names.length === 3 return ,and string
+    //>=4 slice first 2, return first 2 and slicearr.length string
+    if(names.length === 0){
+        return "no one likes this"
+    }
+    if(names.length === 1){
+        return `${names[0]} likes this`
+    }else if(names.length === 2){
+        return `${names[0]} and ${names[1]} like this`
+    }else if(names.length === 3){
+        return `${names[0]}, ${names[1]} and ${names[2]} like this`
+    }else{
+        let first = names.splice(0,2)
+        return `${first[0]}, ${first[1]} and ${names.length} others like this`
+    }
+}
+
+console.log(likes(['John']))// 'John likes this'
+console.log(likes(['John', 'Martha']))// 'John and Martha like this'
+console.log(likes(['John', 'Martha', 'Matthew']))// 'John, Martha and Matthew Like this'
+console.log(likes(['John', 'Martha', 'Matthew', 'Jeffery']))// 'John, Martha and 2 others Like this'
+
 
