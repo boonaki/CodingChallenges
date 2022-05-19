52ecc57879684789a2bd31089a4327eab5e5182b
boonaki <91851563+boonaki@users.noreply.github.com>
2022-05-19 14:03:24 -0400

    added solution to second coding challenge

---
+++ CC2022-05-19/reverselist.js
@@ -0,0 +1,12 @@
+//In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+//P: an array of numbers, only numbers
+//R: return the array but reversed (not sorted)
+//E:
+
+function reverseList(list){
+    return list.reverse()
+}
+
+console.log(reverseList([1,2,3,4]))//[4,3,2,1]
+console.log(reverseList([5,6,7,8]))//[8,7,6,5]
