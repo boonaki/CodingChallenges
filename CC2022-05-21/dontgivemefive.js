180485136b58d8d868a3aa25ec9c2faebabeae7c
boonaki <joshdurr17@gmail.com>
2022-05-21 23:25:09 -0400

    added solution to coding challenge

---
+++ CC2022-05-21/dontgivemefive.js
@@ -0,0 +1,31 @@
+// In this kata you get the start number and the end number of a region and should return the count of all numbers except numbers with a 5 in it. The start and the end number are both inclusive!
+
+// Examples:
+
+// 1,9 -> 1,2,3,4,6,7,8,9 -> Result 8
+// 4,17 -> 4,6,7,8,9,10,11,12,13,14,16,17 -> Result 12
+
+// The result may contain fives. ;-)
+// The start number will always be smaller than the end number. Both numbers can be also negative!
+
+//P: start number and end number, start will be smaller than end but both can be negative
+//R: a number that represents the count of the numbers, result may include a digit 5
+//E:
+
+function dontGiveMeFive(start, end){
+    //create array of numbers from starting to end
+    //loop through array
+    //convert number to string and check if num includes 5
+    //return arraylength
+    let arr = []
+    for(let i = start; i <= end; i++){
+    if(!i.toString().includes(5)){
+      arr.push(i)
+    } 
+    }
+    return arr.length 
+}
+
+console.log(dontGiveMeFive(1, 9)) //8
+console.log(dontGiveMeFive(5, 20)) //14
+
