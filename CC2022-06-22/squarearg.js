8e4df0a3569aeb72931198c2f377e5f0ed840242
boonaki <joshdurr17@gmail.com>
2022-06-22 16:56:10 -0400

    added solution to coding challenge

---
+++ CC2022-06-22/squarearg.js
@@ -0,0 +1,12 @@
+// write a function that takes an argument and returns the square of it.
+
+//P: Given a number, pos or negative, no special cases
+//R: return the argument value squared
+//E:
+
+function squareNum(num){
+    return num**2
+}
+
+console.log(squareNum(3))//9
+console.log(squareNum(4))//16
