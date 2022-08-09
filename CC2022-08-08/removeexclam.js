8daa191e5a5a93548b4c3f5e130dd7740849e091
boonaki <joshdurr17@gmail.com>
2022-08-08 22:35:59 -0400

    added solution

---
+++ CC2022-08-08/removeexclam.js
@@ -0,0 +1,20 @@
+//Write a function that removes all exclamation marks from a sentence
+
+//P: given a string value, never empty but may not contain exclamation marks
+//R: reutnr the string with exclamation marks remvoved
+//E:
+
+function remove(string){
+    //while the last element is === "!"
+    //slice the slice element (0,-1)
+    //return string
+    while(string.slice(-1) === "!"){
+        string = string.slice(0,-1)
+    }
+    return string
+}
+
+console.log(remove("Hello!"))//Hello
+console.log(remove("!!Hello!"))//!!Hello
+console.log(remove("Hello!!!!"))//Hello
+console.log(remove("Hello"))//Hello
