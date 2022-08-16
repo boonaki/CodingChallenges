9b488f0b6db8b574e0fc98c9e2ca39f96acc2e20
boonaki <joshdurr17@gmail.com>
2022-08-16 14:32:30 -0400

    added solution

---
+++ CC2022-08-16/uppercase.js
@@ -0,0 +1,12 @@
+// Write a function which converts the input string to uppercase.
+
+//P: Given a string
+//R: return given string but in all uppercase characters
+//E:
+
+function uppercase(string){
+    return string.toUpperCase()
+}
+
+console.log('hello')//HELLO
+console.log('Hello')//HELLO
