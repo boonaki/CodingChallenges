096d98fc01c5589a2dff8f9442a3cd93f3d1ba85
boonaki <joshdurr17@gmail.com>
2022-09-25 13:03:25 -0400

    redid first solution

--- CC2022-09-25/consecutivestr.js
+++ CC2022-09-25/consecutivestr.js
@@ -4,34 +4,21 @@
 //return the first longest concatenation
 
 function longestConsec(arr, k) {
-    //create array to store concatenated strings
-    //for loop to array length for i
-    //declare count, init to 1
-    //declare temp as first
-    //while count is less than k
-    //add arr[i+k] into temp
-    //increment count
-    //push temp into array
-    //find max length in array
-    //filter elmenents with max length
-    //return first
-    if (arr.length < 1 || k < 1 || arr.length < k) {
-        return ""
-    }
-    let concatarr = []
-    for (let i = 0; i < arr.length; i++) {
-        let count = 1
-        let temp = arr[i]
-        while (count < k) {
-            if (arr[i + count]) {
-                temp = temp + arr[i + count]
-            }
-            count++
+    //define longest string
+    //loop from i to arr length - k
+    //slice arr from i to i + k
+    //join the sliced array
+    //if sliced string length is greater than longest length
+        //assign longest to sliced string
+    //return longest string
+    let longest = ""
+    for(let i = 0; k > 0 && i <= arr.length-k; i++){
+        let temp = arr.slice(i, i+k).join('')
+        if(temp.length > longest.length){
+            longest = temp
         }
-        concatarr.push(temp)
     }
-    concatarr.sort((a, b) => b.length - a.length)
-    return concatarr[0]
+    return longest
 }
 
 console.log(longestConsec(["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], 2))//"folingtrashy"
