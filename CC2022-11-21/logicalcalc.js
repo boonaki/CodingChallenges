7d6075e06b81848360c1a4301c2305b1e7e661db
boonaki <joshdurr17@gmail.com>
2022-11-21 14:16:49 -0500

    added solution

---
+++ CC2022-11-21/logicalcalc.js
@@ -0,0 +1,19 @@
+//given an array o booleans, and a logical operator in type string
+//return a boolean based on sequentially applying the operator to the values
+
+function logicalCalc(bools, op) {
+    //check operator
+    //join bools with corresponding operator
+    //return check from string with eval method
+    if (op === 'AND') {
+        array = array.join(' && ')
+    } else if (op === 'OR') {
+        array = array.join(' || ')
+    } else {
+        array = array.join(' ^ ')
+    }
+    return eval(array) ? true : false
+}
+
+console.log(logicalCalc([true, true, true, false], 'AND'))//false
+console.log(logicalCalc([true, true, true, false], 'OR'))//true
