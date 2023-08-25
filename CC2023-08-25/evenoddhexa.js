1141988e4b758b8429cea764e9715b2b0adaa2ea
boonaki <joshdurr17@gmail.com>
2023-08-25 09:07:40 -0400

    added solution

---
+++ CC2023-08-25/evenoddhexa.js
@@ -0,0 +1,22 @@
+// takes in a number
+// returns a string in binary or hexadecimal based on if the number is even or odd
+
+function NumberToHexOrBi(num){
+   //define string to return
+   // check if number is odd
+   //if so convert to binary
+   //else
+   //convert to hex
+   //return converted string
+   let retStr;
+   if(num % 2 === 0){
+      retStr = num.toString(2);
+   }else{
+      retStr = num.toString(16);
+   }
+   return retStr;
+}
+
+console.log(NumberToHexOrBi(2), "10");
+console.log(NumberToHexOrBi(13), "d");
+console.log(NumberToHexOrBi(47), "2f");
